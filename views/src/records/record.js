e16d83f2bffe235f3e5160119b2afa6a1508a716
Lasha Lezhava <lashalezhava@gmail.com>
2019-01-05 14:27:25 +0400

    Implement grid layout in record

--- views/src/records/record.js
+++ views/src/records/record.js
@@ -1,12 +1,62 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <section id="record">
-    <h1>Title</h1>
-    <span id="category">Gasoline</span>
-    <span id="paidOn">Current Date</span>
-    <span id="amount">Current Date</span>
-    <span id="comment">Comment</span>
-</section>`
+    <div id="title" class="title">Title</div>
+    <paym-tag id="category"></paym-tag>
+    <div id="paidOn">Current Date</div>
+    <div id="amount">Current Date</div>
+    <div id="comment-section">
+         <div class="title">Comment</div>
+         <span id="comment">Comment</span>
+    </div>
+</section>
+<style>
+
+#record {
+    color: #45617e;
+    margin: 1em;
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-template-rows: auto auto auto;
+    grid-gap: 1em;
+    grid-template-areas: 
+    "title date"
+    "category amount"
+    "comment comment"
+}
+
+#title {
+    font-size: 1.5em;
+    grid-area: title;
+    justify-self: start;
+}
+
+#category {
+    grid-area: category;
+    justify-self: start;
+}
+
+#paidOn {
+    grid-area: date;
+    justify-self: end;
+}
+
+#amount {
+    grid-area: amount;
+    justify-self: end
+}
+
+#comment-section {
+    font-size: 1em;
+    grid-area: comment;
+}
+
+.title {
+    font-weight: bold;
+}
+
+</style>
+`
 
 
 // TODO: Add expanded state here.
@@ -33,9 +83,9 @@ class Record extends HTMLElement {
         const date = this.getAttribute('date')
         const comment = this.getAttribute('comment')
 
-        node.querySelector('h1').innerText = title
+        node.querySelector('#title').innerText = title
         node.querySelector('#amount').innerText = amount
-        node.querySelector('#category').innerText = category
+        node.querySelector('#category').setAttribute('text', category)
         node.querySelector('#paidOn').innerText = date
         node.querySelector('#comment').innerText = comment
 
